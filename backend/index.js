15a15ab40b0c0ae60274743ee8bcf723f4b7ac67
Katelyn Chenqi <kchenqi@gmail.com>
2025-12-30 12:23:41 -0800

    Created a backend server.

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,27 @@
-const groups = [];
+const groups = [];                                // stores groups
 
 console.log("Server file started");
 
-const express = require("express");
-const app = express();
+const express = require("express");               // imports express library
+const app = express();                            // creates server application
 
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
+app.use(express.json());                          // incoming requests might contain JSON data (puts parsed data into req.body)
+
+app.get("/home", (req, res) => {
+  res.json({ status:"ok" });                      // sends json response
 });
 
-app.post("/groups", (req, res) => {
-  res.json({
-    message: "Group received",
-    data: req.body
-  });
+app.post("/home", (req, res) => {
+  const group = {
+    id: groups.length + 1,                        // group length + 1
+    name: req.body.name
+  };
+
+  groups.push(group);
+  res.json(group);
 });
 
 
-app.listen(3000, () => {
+app.listen(3000, () => {                          // starts server and opens port 3000
   console.log("Server running on port 3000");
 });
-
-app.use(express.json());
